cb745cd3a8c9c5eb8a4debf42bf9124f0358654b
hassan <hchoudhury20@hotmail.com>
2023-11-30 23:30:12 +0000

    add methods for main method

--- atm/Program.cs
+++ atm/Program.cs
@@ -1,86 +1,115 @@
-﻿public class cardHolder{
+﻿public class CardHolder
+{
     string firstName;
     string secondName;
     int pin;
     double balance;
     string cardNumber;
 
-    public cardHolder(string firstName,string secondName,int pin, double balance,string cardNumber)
+    public CardHolder(string firstName, string secondName, int pin, string cardNumber)
     {
         this.firstName = firstName;
         this.secondName = secondName;
         this.pin = pin;
-        this.balance = balance;
+        this.balance = 0;
         this.cardNumber = cardNumber;
-
     }
 
-    public string getFirstName(){
+    public string GetFirstName()
+    {
         return firstName;
     }
-    public string getSecondName(){
+
+    public string GetSecondName()
+    {
         return secondName;
     }
-    
-    public int getPin()
+
+    public int GetPin()
     {
         return pin;
     }
 
-    public string getCardNumber()
+    public string GetCardNumber()
     {
-        return cardNumber
+        return cardNumber;
     }
-    public getBalance()
+
+    public double GetBalance()
     {
         return balance;
     }
 
-    public void setFirstName(string newFirstName)
+    public void SetFirstName(string newFirstName)
     {
         firstName = newFirstName;
     }
-    public void setSecondName(string newSecondName)
+
+    public void SetSecondName(string newSecondName)
     {
-        secondName = newSecondName; 
+        secondName = newSecondName;
     }
 
-    public void setBalance(double newBalance)
+    public void SetBalance(double newBalance)
     {
         balance = newBalance;
     }
-    public void setPin(int newPin)
+
+    public void SetPin(int newPin)
     {
         pin = newPin;
     }
 
-    public void setCardNumber(string newCardNumber)
+    public void SetCardNumber(string newCardNumber)
     {
         cardNumber = newCardNumber;
     }
-
-    
 }
 
-public static void main(string[] args){
-
-    void showOptions(){
-        Console.WriteLine("1. Check Balance");
-        Console.WriteLine("2. Withdraw");
-        Console.WriteLine("3. Deposit");
-        Console.WriteLine("4. Exit");
-    }
-
-    void checkBalance(cardholder)
+class Program
+{
+    static void Main(string[] args)
     {
-        console.WriteLine($"your balance is {cardholder.getBalance}");
+        void ShowOptions()
+        {
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Deposit");
+            Console.WriteLine("4. Exit");
+        }
+
+        void CheckBalance(CardHolder cardHolder)
+        {
+            Console.WriteLine($"Your balance is {cardHolder.GetBalance()}");
+        }
+
+        void Deposit(CardHolder cardHolder)
+        {
+            Console.WriteLine("How much do you want to deposit?");
+            double depositAmount = Double.Parse(Console.ReadLine());
+            double newBalance = cardHolder.GetBalance() + depositAmount;
+            cardHolder.SetBalance(newBalance);
+        }
+
+        void Withdraw(CardHolder cardHolder)
+        {
+            Console.WriteLine("How much do you want to withdraw?");
+            double withdrawalAmount = double.Parse(Console.ReadLine());
+
+            if (withdrawalAmount > cardHolder.GetBalance()) 
+            { 
+                Console.WriteLine("Not enough funds");
+            }
+            else
+            {
+                double newBalance = cardHolder.GetBalance() - withdrawalAmount;
+                cardHolder.SetBalance(newBalance);
+            }
+        }
+
+        void Balance(CardHolder cardHolder)
+            {
+                Console.WriteLine($"your balance is {cardHolder.GetBalance()}")
+            }   
     }
-
-    void deposit(cardholder)
-    {
-        
-
-    }
-
-
 }
